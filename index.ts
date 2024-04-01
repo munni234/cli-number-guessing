d67c9f424608ed3f871c290acb65f0069001c547
munni234 <161602125+munni234@users.noreply.github.com>
2024-03-31 19:58:02 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+
+
+const randomNumber = Math.floor(Math.random() * 6 + 1)
+
+
+const answer = await inquirer.prompt([
+{
+name: 'userGuessedNumber',
+type:'number',
+message: 'please guess a number  between 1_10:',
+
+},
+]);
+
+
+if(answer.userGuessedNumber === randomNumber) {
+    console.log('congratulation! you guessed right number.');
+} else {
+    console.log('you guessed wrong number');
+}
